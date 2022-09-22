9446462044d0b04d7d4d510a9b90b69928891966
LaiHuuBach31 <hoangbach767@gmail.com>
2022-09-22 13:57:05 +0700

    blog, detail, hơme

---
+++ Pages/blog/blog.js
@@ -0,0 +1,11 @@
+
+// window.addEventListener('scroll',()=>{
+    
+//     console.log(window.scrollY);
+    
+//     if (window.scrollY >= 296) {
+//         document.getElementById('blogs').classList.add('position-fixed')
+//     } else{
+//         document.getElementById('blogs').classList.remove('position-fixed')
+//     }
+// })
